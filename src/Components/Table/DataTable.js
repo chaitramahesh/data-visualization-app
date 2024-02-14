7faa1aa90b2dc2b46d96d49ee2ece196f877d494
chaitramahesh <chaitrah10@gmail.com>
2024-02-14 18:13:21 +0530

    fix: issue in pagination while filtering data

--- src/Components/Table/DataTable.js
+++ src/Components/Table/DataTable.js
@@ -33,20 +33,25 @@ const DataTable = ({ data, selectedProducts, onCheckboxChange }) => {
 
   const totalPages = Math.ceil(filteredData.length / itemsPerPage);
   const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-
-  const visibleData = filteredData.slice(startIndex, endIndex);
+  let endIndex;
+  let visibleData;
+  if (currentPage === totalPages) {
+    endIndex = filteredData.length;
+    visibleData = filteredData.slice(startIndex, endIndex);
+  } else {
+    endIndex = startIndex + itemsPerPage;
+    visibleData = filteredData.slice(startIndex, endIndex);
+  }
 
   return (
     <div className="data-table">
       <h3>Products Data Table</h3>
       <div className="search">
-      <input
-      
-        type="text"
-        placeholder="Search"
-        onChange={(e) => handleSearch(e.target.value)}
-      />
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(e) => handleSearch(e.target.value)}
+        />
       </div>
       <table>
         <thead>
@@ -80,24 +85,22 @@ const DataTable = ({ data, selectedProducts, onCheckboxChange }) => {
       </table>
 
       <div className="pagination">
-        
-          <button
-            onClick={() => handlePageChange(currentPage - 1)}
-            disabled={currentPage === 1}
-          >
-            Previous
-          </button>
-          <span>{`${startIndex + 1}-${endIndex} of ${
-            filteredData.length
-          } `}</span>
-          <button
-            onClick={() => handlePageChange(currentPage + 1)}
-            disabled={currentPage === totalPages}
-          >
-            Next
-          </button>
-        </div>
-    
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <span>{`${startIndex + 1}-${endIndex} of ${
+          filteredData.length
+        } `}</span>
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
